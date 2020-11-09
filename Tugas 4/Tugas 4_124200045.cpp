c9ceb3016218ae57426146e27e4700bcfc7c0c54
aditmulyatama <adit.mlytm21@gmail.com>
2020-11-09 23:22:50 +0700

    Upload tugas 5

--- Tugas 4/Tugas 4_124200045.cpp
+++ Tugas 4/Tugas 4_124200045.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
+ #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
 	int pil;
-	int x,y,hasilKali,hasilPangkat=1;
-;
+	float x,y,hasilKali,hasilPangkat=1;
+
 	cout<<"++++ Perkalian dan Perpangkatakan++++\n";
 	cout<<"1. Perkalian\n2. Perpangkatan\n";
 	cout<<"Pilih : ";cin>>pil;
@@ -12,33 +13,38 @@ int main(){
 		case 1:
 			cout<<"\nInput nilai X : ";cin>>x;
 			cout<<"Input nilai Y : ";cin>>y;
+				cout<<"\nhasil kali "<<x<<" * "<<y<<endl;
 			for(int i=1;i<=x;i++){
 				hasilKali+=y;
-			}
-			cout<<"\nhasil kali "<<x<<" * "<<y<<endl;
-			for(int i=1;i<=x;i++){
-				cout<<y;
+			
 				if(i<x){
-					cout<<" + ";
+					cout<<y<<" + ";
+				} else {
+				
+					cout<<y<<" = "<<hasilKali<<endl;
 				}
-			}
-			cout<<" = "<<hasilKali;
-			break;
+			} 
+		break;
+	
+			
+	
 		
 		case 2:
 			cout<<"\nInput nilai X : ";cin>>x;
 			cout<<"Input nilai Y : ";cin>>y;
+			
 			for(int i=1;i<=y;i++){
 				hasilPangkat*=x;
 			}
 			cout<<"\nhasil pangkat "<<x<<" ^ "<<y<<endl;
+			
 			for(int i=1;i<=y;i++){
 				cout<<x;
 				if(i<y){
 					cout<<" * ";
 				}	
 			}
-			cout<<" = "<<hasilPangkat;	
+			cout<<" = "<<setprecision(11)<<hasilPangkat<<endl;	
 			break;
 		
 		default:
---
+++ Tugas 5/Tugas 5_124200045.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int pilih;
+	cout<<"======Bentuk Generator======\n";
+	cout<<"1. Kotak tengahnya kosong\n";
+	cout<<"2. Segitiga isinya angka ngurut\n";
+	cout<<"Pilih satu: "; cin>>pilih;
+	
+	 switch (pilih){
+	 	case 1:
+	 		
+	 		int p,L,x,y;
+			cout<<"\npanjang : "; cin>>p;
+			cout<<"lebar : "; cin>>L;
+			cout<<endl;
+	
+	 		for(y=1;y<=L;y++){
+				for(x=1;x<=p;x++){
+					if((y>1&&y<L)&&(x>1&&x<p)){
+						cout<<" ";
+					} else {
+						cout<<"*";
+					}
+				}
+				cout<<endl;
+			}
+		break;
+			
+		case 2:
+			int t,i,j,k,jml;
+			cout<<"tinggi : "; cin>>t;
+			for(i=1;i<=t;i++){
+				jml=t-1;
+				k=i;
+				for(j=1;j<=i;j++){
+					if(j>1){
+						k+=jml;
+						cout<<k<<" ";
+						jml--;
+					} else {
+						cout<<i<<" ";
+					}
+				}
+				cout<<endl;
+			}
+		break;
+			
+		default:
+			cout<<"\nsalah input";
+			
+	}
+	system("pause");
+}
+	
